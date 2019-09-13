121ad6a29af4a8e41e6d1dc599158c8dcd77d937
Ghaffaru <mudashiruagm@gmail.com>
2019-09-13 23:36:48 +0000

    admin

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Question
 from .models import Choice
 # Register your models here.
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
